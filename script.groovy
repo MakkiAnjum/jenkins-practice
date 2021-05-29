35ca9a89da6399aeb735f0c588218ed92a4af3e6
Makki Anjum <40621334+MakkiAnjum@users.noreply.github.com>
2021-05-29 17:31:06 +0100

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildApp() {
+  echo 'Building the application'
+}
+
+def testApp() {
+  echo 'testing the application'
+}
+
+def deployApp() {
+  echo 'deploying the application'
+}
+
+return this
