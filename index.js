f774d70d3b60baa0cc9f77bf0a30f5a3540a8bbe
MakkiAnjum <muhammadmakkianjum@yahoo.com>
2021-05-29 16:12:56 +0100

    Init

---
+++ index.js
@@ -0,0 +1,3 @@
+const app = require('express')();
+
+app.listen(7000, () => console.log('Listening on port 7000'))
---
+++ simple.test.js
@@ -0,0 +1,5 @@
+test('This is my test', () => {
+  const a = 1 + 1;
+
+  expect(a).toEqual(2);
+});
